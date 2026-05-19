8d9454cf1c86c45712f15aea992b2d68141dbb75
Jacob-Clay <clayjac000@gmail.com>
2026-05-19 10:01:25 +0200

    Use Aur and update Arch list

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
 install_on_fedora() {
-    sudo dnf install -y ansible
+  sudo dnf install -y ansible
 }
 
 install_on_ubuntu() {
-    sudo apt-get update
-    sudo apt-get install -y ansible
+  sudo apt-get update
+  sudo apt-get install -y ansible
 }
 
 install_on_pacman() {
-    sudo pacman -S --noconfirm --needed ansible
+  sudo pacman -S --noconfirm --needed ansible
 }
 
 install_on_mac() {
-    brew install ansible
+  brew install ansible
 }
 
 OS="$(uname -s)"
 case "${OS}" in
-    Linux*)
-        if [ -f /etc/fedora-release ]; then
-            install_on_fedora
-        elif [ -f /etc/lsb-release ]; then
-            install_on_ubuntu
-        elif [ -f /etc/pacman.conf ]; then
-            install_on_pacman
-        else
-            echo "Unsupported Linux distribution"
-            exit 1
-        fi
-        ;;
-    Darwin*)
-        install_on_mac
-        ;;
-    *)
-        echo "Unsupported operating system: ${OS}"
-        exit 1
-        ;;
+Linux*)
+  if [ -f /etc/fedora-release ]; then
+    install_on_fedora
+  elif [ -f /etc/lsb-release ]; then
+    install_on_ubuntu
+  elif [ -f /etc/pacman.conf ]; then
+    install_on_pacman
+  else
+    echo "Unsupported Linux distribution"
+    exit 1
+  fi
+  ;;
+Darwin*)
+  install_on_mac
+  ;;
+*)
+  echo "Unsupported operating system: ${OS}"
+  exit 1
+  ;;
 esac
 
-
+ansible-galaxy collection install -r ~/.bootstrap/requirements.yml
 ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
 
 echo "Ansible installation complete."
